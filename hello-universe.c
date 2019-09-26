b1971807af081fb1d66162f7088df72c709b5fd9
kaylavie <37421434+kaylavie@users.noreply.github.com>
2019-09-26 19:58:47 -0400

    Update hello-universe.c

--- hello-universe.c
+++ hello-universe.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 
-int main()
+int main()                                      // program entry point
 {
-        printf("\nHello Universe!\n\n");
-        return 0;
+        /* Arithmetic Types:
+        char: 1 byte
+        int: 4 bytes in 32-bit env
+        float: 4 bytes
+        double: 8 bytes
+        */
+        
+        /* Size 
+        short: 2 bytes
+        long: 4 bytes
+        long long: 8 bytes
+        */
+        
+        const float pi = 3.14159f;      // const =  unmodifiable variable
+        float radius;   
+        printf("\nHello Universe!\n\n");       
+        printf("Enter radius: ");
+        scanf("%f", radius);
+        area = pi * radious * radius    // calculate area from radius
+        printf("Area = %f\n", area);    
+     
+        return 0;                               // transfering control back to operating system
+           
 }
+
+
