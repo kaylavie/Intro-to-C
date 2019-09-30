ffde2566dabff38a2eb747170a2535500f55b7ea
kaylavie <37421434+kaylavie@users.noreply.github.com>
2019-09-30 06:44:42 -0400

    Update hello-universe.c

--- hello-universe.c
+++ hello-universe.c
@@ -2,19 +2,7 @@
 
 int main()                                      // program entry point
 {
-        /* Arithmetic Types:
-        char: 1 byte
-        int: 4 bytes in 32-bit env
-        float: 4 bytes
-        double: 8 bytes
-        */
-        
-        /* Size 
-        short: 2 bytes
-        long: 4 bytes
-        long long: 8 bytes
-        */
-        
+       
         const float pi = 3.14159f;      // const =  unmodifiable variable
         float radius;   
         printf("\nHello Universe!\n\n");       
